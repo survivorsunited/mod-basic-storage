9b74e0c17b6518a169480692e43a1d91786398aa
Max Barrass <max.barrass@gmail.com>
2026-06-29 03:06:36 +1000

    Register crate front renderer

--- src/client/java/com/khazoda/basicstorage/BasicStorageClient.java
+++ src/client/java/com/khazoda/basicstorage/BasicStorageClient.java
@@ -1,9 +1,12 @@
 package com.khazoda.basicstorage;
 
 import com.khazoda.basicstorage.config.ConfigSyncPayload;
+import com.khazoda.basicstorage.registry.BlockEntityRegistry;
+import com.khazoda.basicstorage.renderer.CrateBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class BasicStorageClient implements ClientModInitializer {
   @Override
@@ -17,5 +20,7 @@ public class BasicStorageClient implements ClientModInitializer {
     ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
       BasicStorageConfig.getInstance().load();
     });
+
+    BlockEntityRendererFactories.register(BlockEntityRegistry.CRATE_BLOCK_ENTITY, CrateBlockEntityRenderer::new);
   }
 }
