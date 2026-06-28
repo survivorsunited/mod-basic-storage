1b756fbbae02de20b61c04f1cc424b8c8a54dd3f
Max Barrass <max.barrass@gmail.com>
2026-06-28 22:10:33 +1000

    Update player world access

--- src/main/java/com/khazoda/basicstorage/util/BlockUtils.java
+++ src/main/java/com/khazoda/basicstorage/util/BlockUtils.java
@@ -14,8 +14,7 @@ public class BlockUtils {
     final double castLength = Vec3d.ofCenter(target).subtract(castOrigin).length() + 1;
     final Vec3d playerRotation = player.getRotationVector();
     final Vec3d castTarget = castOrigin.add(playerRotation.multiply(castLength));
-    
-    return player.getWorld().raycast(new RaycastContext(castOrigin, castTarget, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, player));
+    return player.getEntityWorld().raycast(new RaycastContext(castOrigin, castTarget, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, player));
   }
 
   public static int getComparatorOutputStrength(int itemStackCount) {
