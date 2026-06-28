292daafb23155de0b5f05002875f832d711f8d6b
Max Barrass <max.barrass@gmail.com>
2026-06-28 22:10:10 +1000

    Use Fabric block entity builder

--- src/main/java/com/khazoda/basicstorage/registry/BlockEntityRegistry.java
+++ src/main/java/com/khazoda/basicstorage/registry/BlockEntityRegistry.java
@@ -4,6 +4,7 @@ import com.khazoda.basicstorage.BasicStorage;
 import com.khazoda.basicstorage.Constants;
 import com.khazoda.basicstorage.block.entity.CrateBlockEntity;
 import com.khazoda.basicstorage.block.entity.CrateStationBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
@@ -15,11 +16,11 @@ public class BlockEntityRegistry {
 
   public static final BlockEntityType<CrateBlockEntity> CRATE_BLOCK_ENTITY = Registry.register(
       Registries.BLOCK_ENTITY_TYPE, Identifier.of(Constants.BS_NAMESPACE, "crate_block_entity"),
-      BlockEntityType.Builder.create(CrateBlockEntity::new,
+      FabricBlockEntityTypeBuilder.create(CrateBlockEntity::new,
           BlockRegistry.CRATE_BLOCK).build());
   public static final BlockEntityType<CrateStationBlockEntity> CRATE_STATION_BLOCK_ENTITY = Registry.register(
       Registries.BLOCK_ENTITY_TYPE, Identifier.of(Constants.BS_NAMESPACE, "crate_station_block_entity"),
-      BlockEntityType.Builder.create(CrateStationBlockEntity::new,
+      FabricBlockEntityTypeBuilder.create(CrateStationBlockEntity::new,
           BlockRegistry.CRATE_STATION_BLOCK).build());
 
 
