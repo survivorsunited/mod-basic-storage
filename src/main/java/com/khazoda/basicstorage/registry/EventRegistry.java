ed63993b11b153351668ae162a5a626f8c5f189c
Max Barrass <max.barrass@gmail.com>
2026-07-05 20:35:31 +1000

    Register crate transfer handler

--- src/main/java/com/khazoda/basicstorage/registry/EventRegistry.java
+++ src/main/java/com/khazoda/basicstorage/registry/EventRegistry.java
@@ -1,15 +1,13 @@
 package com.khazoda.basicstorage.registry;
-
 import com.khazoda.basicstorage.BasicStorage;
 import com.khazoda.basicstorage.block.CrateBlock;
 import com.khazoda.basicstorage.block.CrateStationBlock;
-
-
+import com.khazoda.basicstorage.block.CrateTransferHandler;
 public class EventRegistry {
   public static void init() {
+    CrateTransferHandler.init();
     CrateBlock.initOnUseMethod();
     CrateStationBlock.initOnUseMethod();
-
     BasicStorage.loadedRegistries += 1;
   }
 }
