4d5ebd43b259c67d22b29a966c43d745295f21fc
Max Barrass <max.barrass@gmail.com>
2026-07-05 20:35:09 +1000

    Use custom crate item

--- src/main/java/com/khazoda/basicstorage/util/Reggie.java
+++ src/main/java/com/khazoda/basicstorage/util/Reggie.java
@@ -1,6 +1,8 @@
 package com.khazoda.basicstorage.util;
 
 import com.khazoda.basicstorage.Constants;
+import com.khazoda.basicstorage.block.CrateBlock;
+import com.khazoda.basicstorage.item.CrateBlockItem;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -10,41 +12,18 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class Reggie {
-//  /* Register block and item with default item settings */
-//  public static <B extends Block> B register(String name, B block, Item.Settings itemSettings) {
-//    return RegistryHelper.registerBlock(name, block, itemSettings);
-//  }
-//
-//  /* Register block *with* corresponding item*/
-//  public static <I extends BlockItem> BlockItem register(String name, I blockItem) {
-//    return RegistryHelper.registerBlockItem(name, blockItem);
-//  }
-//
-//  /* Register block *without* corresponding item */
-//  public static <B extends Block> B register(String name, B block) {
-//    return RegistryHelper.registerBlockOnly(name, block);
-//  }
-//
-//  /* Register item */
-//  public static Item register(String name) {
-//    return RegistryHelper.registerItem(name, new Item(new Item.Settings().maxCount(64)));
-//  }
-
-  // General use Identifier() maker function
   public static Identifier newID(String name) {
     return Identifier.of(Constants.BS_NAMESPACE, name);
   }
 
-  // Block Registry Helper Functions
-  // *******************************
-  // 1. Default BlockItem Registration Entrypoint: creates Identifier from ModID & block name
   public static <B extends Block> B register(String name, B block, Item.Settings itemSettings) {
     return register(newID(name), block, itemSettings);
   }
 
-  // 2. Takes identifier and registers block and block items
   public static <B extends Block> B register(Identifier name, B block, Item.Settings itemSettings) {
-    BlockItem item = new BlockItem(block, itemSettings);
+    BlockItem item = block instanceof CrateBlock
+        ? new CrateBlockItem(block, itemSettings)
+        : new BlockItem(block, itemSettings);
     item.appendBlocks(Item.BLOCK_ITEMS, item);
 
     Registry.register(Registries.BLOCK, name, block);
@@ -75,8 +54,6 @@ public class Reggie {
     return itemGroup;
   }
 
-  // Item Registry Helper Functions
-  // ******************************
   public static <I extends Item> I register(String name, I item) {
     return Registry.register(Registries.ITEM, newID(name), item);
   }
