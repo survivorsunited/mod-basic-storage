64d6218acbb809b06d542fa395f37dd2c9034495
Max Barrass <max.barrass@gmail.com>
2026-07-09 21:29:30 +1000

    Register crate item special renderer

--- src/client/java/com/khazoda/basicstorage/BasicStorageClient.java
+++ src/client/java/com/khazoda/basicstorage/BasicStorageClient.java
@@ -3,14 +3,19 @@ package com.khazoda.basicstorage;
 import com.khazoda.basicstorage.config.ConfigSyncPayload;
 import com.khazoda.basicstorage.registry.BlockEntityRegistry;
 import com.khazoda.basicstorage.renderer.CrateBlockEntityRenderer;
+import com.khazoda.basicstorage.renderer.CrateItemSpecialRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.client.render.item.model.special.SpecialModelTypes;
+import net.minecraft.util.Identifier;
 
 public class BasicStorageClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
+    SpecialModelTypes.ID_MAPPER.put(Identifier.of(Constants.BS_NAMESPACE, "crate_renderer"), CrateItemSpecialRenderer.Unbaked.CODEC);
+
     ClientPlayNetworking.registerGlobalReceiver(ConfigSyncPayload.ID, (payload, context) -> {
       context.client().execute(() -> {
         BasicStorageConfig.getInstance().setBreakWithAxeOnly(payload.breakWithAxeOnly());
