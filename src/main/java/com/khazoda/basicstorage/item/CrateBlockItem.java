9732c46711e2aacee1467156db43d004885f705c
Max Barrass <max.barrass@gmail.com>
2026-07-09 20:34:31 +1000

    Fix crate tooltip signature for 1.21.11

--- src/main/java/com/khazoda/basicstorage/item/CrateBlockItem.java
+++ src/main/java/com/khazoda/basicstorage/item/CrateBlockItem.java
@@ -5,6 +5,7 @@ import com.khazoda.basicstorage.structure.CrateSlotComponent;
 import com.khazoda.basicstorage.util.NumberFormatter;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.minecraft.block.Block;
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,7 +13,7 @@ import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
-import java.util.List;
+import java.util.function.Consumer;
 
 public class CrateBlockItem extends BlockItem {
   public CrateBlockItem(Block block, Item.Settings settings) {
@@ -20,7 +21,7 @@ public class CrateBlockItem extends BlockItem {
   }
 
   @Override
-  public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+  public void appendTooltip(ItemStack stack, Item.TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type) {
     CrateSlotComponent contents = stack.get(DataComponentRegistry.CRATE_CONTENTS);
     if (contents == null || contents.count() <= 0 || contents.item().isBlank()) {
       return;
@@ -30,7 +31,7 @@ public class CrateBlockItem extends BlockItem {
     MutableText countLine = Text.literal("x" + NumberFormatter.toFormattedNumber(contents.count())).withColor(0xFFDD99);
     MutableText itemLine = Text.literal(item.getItem().getName().getString()).withColor(0xCCAA77);
 
-    tooltip.add(countLine);
-    tooltip.add(itemLine);
+    textConsumer.accept(countLine);
+    textConsumer.accept(itemLine);
   }
 }
